ba75dd0a8eee18bb910fd83db3295f553f7c3196
mercyogalo <ogalomercy8@gmail.com>
2025-04-22 00:44:21 +0300

    Created the components for the form and the tasks

--- my-app/src/Todo.js
+++ my-app/src/Todo.js
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from '@mui/icons-material/Delete';
+import TextField from "@mui/material/TextField";
 
 
 
@@ -16,20 +17,18 @@ export default function Todo(){
 
 
 
-const [task,setTask]=useState(null);
+const [task,setTask]=useState(false);
+
+function taskhandleChange(e){
+  setTask(e.target.value);
+}
 
 
-
-    return (
-        <Box component="section" sx={{ p: 2, border: '1px dashed grey' , width:1000, height:1000, margin:"auto"}}>
-        
-        <Card sx={{ maxWidth: 800 , marginTop: 2}}>
+let taskCard=(
+   <Card sx={{ maxWidth: 800 , marginTop: 2}}>
       <CardActionArea>
        
         <CardContent>
-
-        
-
           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
             Task 1 here 
           </Typography>
@@ -42,10 +41,35 @@ const [task,setTask]=useState(null);
      
     </Card>
 
+)
+
+let taskForm=(
+<Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '88ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      
+      <TextField id="standard-basic" label="Add task" variant="standard" onChange={taskhandleChange}/>
+    </Box>
+)
+
+
+    return (
 
 
 
 
-      </Box>
+
+
+        <Box component="section" sx={{ p: 2, border: '1px dashed grey' , width:1000, height:1000, margin:"auto"}}>
+        {taskCard}
+        {taskForm}
+
+        </Box>
+
+
+
     );
 }
